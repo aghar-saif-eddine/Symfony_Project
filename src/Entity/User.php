90abde63b7d640f73a052e9ffdf53bdff6cbe896
aghar-saif-eddine <saifeddine.aghar@isimg.tn>
2021-08-06 15:44:04 +0100

    Symfony project

---
+++ src/Entity/User.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface; 
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(
+ *  fields={"email"},
+ *  message="l\'email que vous saisi est deja utilise"
+ * )
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * 
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *@Assert\Length(
+     *      min =9,
+     *      max = 50,
+     *      minMessage = "Your first name must be at least {{ limit }} characters long",
+     *@Assert\EqualTo(propertyPath="confirmpass")
+     */
+    private $password;
+
+    /**
+     *@Assert\EqualTo(propertyPath="password")
+     */
+    public $confirmpass;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+    
+    public function eraseCredentials(){}
+
+    public  function getSalt() {}
+
+    public function getRoles(){
+        return ['ROLE_USER'];
+    }
+}
